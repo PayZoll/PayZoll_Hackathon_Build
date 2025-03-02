1b1e2dcf6073a2d9ec85b93b1ccc464169c21099
18Abhianv07 <abhinavpangaria2003@gmail.com>
2025-03-02 19:08:52 +0530

    backend security check and api test-final

--- Frontend/src/constant/domain.js
+++ Frontend/src/constant/domain.js
@@ -1,3 +1,1 @@
-// export const backendDomain = "https://payzoll-educhain.onrender.com";
 export const backendDomain = "https://payzoll-sonic-build.onrender.com";
-// export const backendDomain = "http://localhost:5001";
