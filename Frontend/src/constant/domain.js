2ed859648a01b3e88cacde45b2be0ecc0fcd3254
18Abhianv07 <abhinavpangaria2003@gmail.com>
2025-03-02 18:34:44 +0530

    backend server changes

--- Frontend/src/constant/domain.js
+++ Frontend/src/constant/domain.js
@@ -1,2 +1,3 @@
-export const backendDomain = "https://payzoll-educhain.onrender.com";
+// export const backendDomain = "https://payzoll-educhain.onrender.com";
+export const backendDomain = "https://payzoll-sonic-build.onrender.com";
 // export const backendDomain = "http://localhost:5001";
